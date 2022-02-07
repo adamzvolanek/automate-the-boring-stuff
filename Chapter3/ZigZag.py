5ab9650017428577a8016b662e312e5ebb988db4
waddoo123 <waddoo123+github@gmail.com>
2022-02-06 20:43:36 -0600

    Beginnings of Chapter 3 and ZigZag file practice with two different methods of operation.

---
+++ Chapter3/ZigZag.py
@@ -0,0 +1,46 @@
+# Small animation program to create a back-and-forth zigzag pattern
+# Will operate until stop is requested or Ctrl+C
+
+import sys
+
+
+# String to print
+def zigzag():
+    print('********')
+    return None
+
+indent = 0
+indentIncreasing = True
+
+try:
+    while True:
+        print(' ' * indent, end='')
+        print('********')
+
+        if indentIncreasing:
+            indent = indent + 1
+            if indent == 20:
+                indentIncreasing = False
+
+        else:
+            indent = indent - 1
+
+            if indent == 0:
+                indentIncreasing = True
+
+except KeyboardInterrupt:
+    pass
+
+try:
+    while True:
+        for i in range(19):
+            print(' ' * indent, end='')
+            print('xxxxxxxxx')
+            indent = i + 1
+        for i in range(19, 0, -1):
+            print(' ' * indent, end='')
+            print('xxxxxxxxx')
+            indent = i - 1
+
+except KeyboardInterrupt:
+    sys.exit()
